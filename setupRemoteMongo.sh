3eb6c6b6cd4cb9c3227305a2199297be65dccbbb
Sai Kiran <103346916+saik64757@users.noreply.github.com>
2023-04-10 23:54:05 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,2 +1,2 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb+srv://xflix:xflix@xflix.fqn2x9l.mongodb.net/xflix?retryWrites=true&w=majority" --drop --collection videos --file data/xflix_data.json
+mongoimport --uri "mongodb+srv://xflix:xflix@xflix.dziefqv.mongodb.net/xflix?retryWrites=true&w=majority" --drop --collection videos --file data/xflix_data.json
